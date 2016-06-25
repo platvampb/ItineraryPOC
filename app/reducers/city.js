880d1dcb3bf505f22900eaf7b983f160cfbc013e
Bo Zhang <vampireb@gmail.com>
2016-06-24 22:35:49 -0400

    remove redundant reducer file.

--- app/reducers/city.js
+++
@@ -1,9 +0,0 @@
-function city(state, action) {
-	switch (action.type) {
-		case RECEIVE_SEARCH_CITY:
-		return Object.assign(state, {display_description: highlightSearchResult(state)});
-
-		default:
-		return state
-	}
-}
