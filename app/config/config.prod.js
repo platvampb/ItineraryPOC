a310efa0b91a0c292acb457331a932b4b60bd73a
Bo Zhang <vampireb@gmail.com>
2016-10-12 20:29:15 -0400

    Establish new convetion where all urls end with '\'

--- app/config/config.prod.js
+++ app/config/config.prod.js
@@ -1,13 +1,13 @@
 export const mallocApi = {
-	baseUrl: "https://malloc.wewherego.com/malloc/api",
-	regionSearch: "/regions/places/search",
-	tripGet: "/trips/get",
-	command: "/command/index",
-	cdnUrl: "https://s3.amazonaws.com",
+	baseUrl: "https://malloc.wewherego.com/malloc/api/",
+	regionSearch: "regions/places/search/",
+	tripGet: "trips/get/",
+	command: "command/index/",
+	cdnUrl: "https://s3.amazonaws.com/",
 }
 
 export const homeApi = {
-	baseUrl: "https://www.wewherego.com",
-	signIn: "/user/signin",
-	signOut: "/user/signout",
+	baseUrl: "https://www.wewherego.com/",
+	signIn: "user/signin/",
+	signOut: "user/signout/",
 }
