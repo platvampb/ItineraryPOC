18101fef699c9f23531a72e926d686c29d83caa4
Bo Zhang <vampireb@gmail.com>
2016-10-13 07:18:52 -0400

    Revert some of the previous changes by removing '\' at the end of path names.

--- app/config/config.prod.js
+++ app/config/config.prod.js
@@ -1,13 +1,13 @@
 export const mallocApi = {
 	baseUrl: "https://malloc.wewherego.com/malloc/api/",
-	regionSearch: "regions/places/search/",
-	tripGet: "trips/get/",
-	command: "command/index/",
+	regionSearch: "regions/places/search",
+	tripGet: "trips/get",
+	command: "command/index",
 	cdnUrl: "https://s3.amazonaws.com/",
 }
 
 export const homeApi = {
 	baseUrl: "https://www.wewherego.com/",
-	signIn: "user/signin/",
-	signOut: "user/signout/",
+	signIn: "user/signin",
+	signOut: "user/signout",
 }
